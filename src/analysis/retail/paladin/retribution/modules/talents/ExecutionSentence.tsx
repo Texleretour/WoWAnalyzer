eaef7d0a4304661748d513a060e544edbb779874
Texleretour <72305552+Texleretour@users.noreply.github.com>
2026-03-24 02:50:22 +0100

    add execution sentence unused gcd analysis and timeline

--- src/analysis/retail/paladin/retribution/modules/talents/ExecutionSentence.tsx
+++ src/analysis/retail/paladin/retribution/modules/talents/ExecutionSentence.tsx
@@ -1,12 +1,17 @@
 import items from 'common/ITEMS/deathknight';
+import SPELLS from 'common/SPELLS';
 import { TALENTS_PALADIN } from 'common/TALENTS';
-import { CastDetail, GuideSection, PerCastData } from 'interface/guide/components';
+import { CastDetail, CastInSequence, GuideSection, PerCastData } from 'interface/guide/components';
+import { SpellSequence } from 'interface/guide/components/CastSequence';
 import SpellLink from 'interface/SpellLink';
+import { spellName } from 'interface/Table/ThroughputTable';
 import Analyzer, { Options, SELECTED_PLAYER } from 'parser/core/Analyzer';
 import Events, {
   AnyEvent,
   CastEvent,
   DamageEvent,
+  EventType,
+  FightEndEvent,
   GlobalCooldownEvent,
   RemoveBuffEvent,
 } from 'parser/core/Events';
@@ -33,7 +38,6 @@ interface ExecutionSentenceCooldownCast extends CooldownTrigger<CastEvent> {
 const GCD_TOLERANCE = 25;
 
 class ExecutionSentence extends MajorCooldown<ExecutionSentenceCooldownCast> {
-  #executionSentenceCasts: ExecutionSentenceCooldownCast[] = [];
   #activeWindow: ExecutionSentenceCooldownCast | null = null;
   #globalCooldownEnds = 0;
 
@@ -46,12 +50,14 @@ class ExecutionSentence extends MajorCooldown<ExecutionSentenceCooldownCast> {
       Events.cast.by(SELECTED_PLAYER).spell(TALENTS_PALADIN.EXECUTION_SENTENCE_TALENT),
       this.#onExecutionSentenceCast,
     );
+    this.addEventListener(
+      Events.removebuff.by(SELECTED_PLAYER).spell(SPELLS.EXECUTION_SENTENCE_BUFF),
+      this.#onExecutionSentenceEnd,
+    );
+    this.addEventListener(Events.fightend, this.#onExecutionSentenceEnd);
 
     this.addEventListener(Events.cast.by(SELECTED_PLAYER), this.#onCast);
-
     this.addEventListener(Events.GlobalCooldown.by(SELECTED_PLAYER), this.#onGlobalCooldown);
-
-    this.addEventListener(Events.removebuff.by(SELECTED_PLAYER), this.#onExecutionSentenceEnd);
   }
 
   #onExecutionSentenceCast(event: CastEvent) {
@@ -66,27 +72,28 @@ class ExecutionSentence extends MajorCooldown<ExecutionSentenceCooldownCast> {
     };
   }
 
-  #onExecutionSentenceEnd(event: RemoveBuffEvent) {
+  #onExecutionSentenceEnd(event: RemoveBuffEvent | FightEndEvent) {
     if (!this.#activeWindow) {
       return;
     }
+    this.#activeWindow.timeline.end = event.timestamp;
 
     this.recordCooldown(this.#activeWindow);
     this.#activeWindow = null;
   }
 
   #onCast(event: CastEvent) {
-    if (!this.#activeWindow) {
+    if (!this.#activeWindow || event.ability.guid === SPELLS.MELEE.id || !event.globalCooldown) {
       return;
     }
 
     this.#activeWindow.unusedGcdTime += Math.max(event.timestamp - this.#globalCooldownEnds, 0);
+    this.#activeWindow.timeline.events.push(event);
   }
 
   #onGlobalCooldown(event: GlobalCooldownEvent) {
     this.#globalCooldownEnds = event.duration + event.timestamp;
     if (this.#activeWindow) {
-      this.#activeWindow.timeline.events?.push(event);
       this.#activeWindow.globalCooldowns.push(event.duration);
     }
   }
@@ -103,6 +110,23 @@ class ExecutionSentence extends MajorCooldown<ExecutionSentenceCooldownCast> {
     return Math.max(Math.floor(cast.unusedGcdTime / avgGcd), 0);
   }
 
+  #buildSpellSequence(cast: ExecutionSentenceCooldownCast): CastInSequence[] {
+    return cast.timeline.events
+      .filter((event): event is CastEvent => event.type === EventType.Cast)
+      .map((event) => ({
+        timestamp: event.timestamp,
+        spellId: event.ability.guid,
+        spellName: event.ability.name,
+        icon: event.ability.abilityIcon.replace('.jpg', ''),
+        tooltip: (
+          <>
+            <SpellLink spell={event.ability.guid} />
+            <div>@ {this.owner.formatTimestamp(event.timestamp)}</div>
+          </>
+        ),
+      }));
+  }
+
   description(): ReactNode {
     return (
       <>
@@ -112,21 +136,24 @@ class ExecutionSentence extends MajorCooldown<ExecutionSentenceCooldownCast> {
   }
 
   #gcdPerformance(cast: ExecutionSentenceCooldownCast): ChecklistUsageInfo {
-    const unusedGcds = this.#getUnusedGlobalCooldowns(cast);
+    const avgGcd = this.#getAverageGcdOfWindow(cast);
+    const unusedGlobalCooldowns = Math.max(Math.floor(cast.unusedGcdTime / avgGcd), 0);
+    const estimatedPotentialCasts = (cast.timeline.end! - cast.timeline.start) / avgGcd;
+    const gcdPerfCalc = (unusedGlobalCooldowns / estimatedPotentialCasts) * 100;
 
     return {
       check: 'global-cooldown',
       timestamp: cast.event.timestamp,
       performance: evaluateQualitativePerformanceByThreshold({
-        actual: unusedGcds,
-        isLessThan: {
-          perfect: 0,
-          good: 1,
-          ok: 3,
+        actual: gcdPerfCalc,
+        isLessThanOrEqual: {
+          perfect: 7.5,
+          good: 15,
+          ok: 25,
         },
       }),
-      details: <>{unusedGcds} unused global cooldowns</>,
-      summary: <>gcd summarysdsd</>,
+      details: <div>{unusedGlobalCooldowns} unused global cooldowns</div>,
+      summary: <div>gcd summarysdsd</div>,
     };
   }
 
@@ -149,19 +176,28 @@ class ExecutionSentence extends MajorCooldown<ExecutionSentenceCooldownCast> {
   #buildPerCastData(): PerCastData[] {
     return this.casts.map((cast) => {
       const spellUse = this.explainPerformance(cast);
+      const castSequence = this.#buildSpellSequence(cast);
+      console.log('sequence', castSequence);
+      const unusedGlobalCooldowns = this.#getUnusedGlobalCooldowns(cast);
 
       return {
         performance: spellUse.performance,
         timestamp: this.owner.formatTimestamp(cast.event.timestamp),
         stats: [
           {
-            value: this.#getUnusedGlobalCooldowns(cast),
+            value: unusedGlobalCooldowns,
             label: 'Unused GCDs',
             tooltip: <>Estimated unused global cooldowns during this window.</>,
             performance: this.#gcdPerformance(cast).performance,
           },
         ],
-        details: <>fdsfisd</>,
+        additionalContent:
+          castSequence.length > 0
+            ? {
+                title: 'Cast Sequence',
+                content: <SpellSequence casts={castSequence} iconSize={40} />,
+              }
+            : undefined,
       };
     });
   }
